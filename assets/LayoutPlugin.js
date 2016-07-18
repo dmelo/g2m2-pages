82cb07bcf48308a1de12f899b09aa5b16fa0ea36
Diogo Oliveira de Melo <dmelo87@gmail.com>
2016-07-18 16:38:49 -0300

    minor layout change

--- assets/LayoutPlugin.js
+++ assets/LayoutPlugin.js
@@ -6,8 +6,8 @@
 
 
     LayoutPlugin.prototype.postHtmlCalc = function (html) {
-        var header = "\n<div class=\"container\">\n <div class=\"row\">\n <ul class=\"nav nav-pills nav-stacked span2\">\n <li class=\"sidebar-brand\">\n <a href=\"/\">Diogo Melo</a>\n </li>\n \n <li><a href=\"/\">Welcome</a></li>\n <li><a href=\"/projects\">Projects</a></li>\n <li><a href=\"/me\">Me</a></li>\n <li><a href=\"/blog/\">Blog</a></li>\n <li class=\"ads\"><a href=\"http://mdownhost.me\" target=\"_blank\"><button>MDownHost.ME</button><p><small>CMS for developers</small></p></a></li>\n <li class=\"ads\"><a href=\"http://amuzi.me\" target=\"_blank\"><button>AMUZI</button><p><small>The open source way to enjoy music</small></p></a></li>\n </ul>\n </div>",
-            footer = "\n</div><footer>\n <div class=\"container\">\n <p id=\"legal\">Powered by <a target=\"_blank\" href=\"http://g2m2.net\">G2M2.net</a> - Copyright &copy; 2011-2016 Diogo Oliveira de Melo.</p>\n </div>\n</footer>";
+        var header = "<div class=\"container\"> <div class=\"row\"> <div class=\"col-md-3\" role=\"complementary\"> <ul class=\"nav nav-pills nav-stacked span2\"> <li class=\"sidebar-brand\"> <a href=\"/\">Diogo Melo</a> </li>  <li><a href=\"/\">Welcome</a></li> <li><a href=\"/projects\">Projects</a></li> <li><a href=\"/me\">Me</a></li> <li><a href=\"/blog/\">Blog</a></li> <li class=\"ads\"><a href=\"http://mdownhost.me\" target=\"_blank\"><button>MDownHost.ME</button><p><small>CMS for developers</small></p></a></li> <li class=\"ads\"><a href=\"http://amuzi.me\" target=\"_blank\"><button>AMUZI</button><p><small>The open source way to enjoy music</small></p></a></li> </ul> </div> <div class=\"col-md-9\" role=\"main\">",
+            footer = "</div> </div> <footer> <div class=\"container\"> <p id=\"legal\">Powered by <a target=\"_blank\" href=\"http://g2m2.net\">G2M2.net</a> - Copyright &copy; 2011-2016 Diogo Oliveira de Melo.</p> </div></footer>";
 
         return header + html + footer;
     };
